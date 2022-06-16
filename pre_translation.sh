5b2511344527edc0e979a651d465f9932bde6b90
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-16 15:21:05 -0400

    Update pre_translation.sh

--- pre_translation.sh
+++ pre_translation.sh
@@ -6,7 +6,7 @@ make docker-compose-sources
 bash -c "docker-compose -f tools/docker-compose/_sources/docker-compose.yml run awx_1 make awx-link migrate po messages"
 
 # Move extracted Strings to Translation Directory
-mv awx/locale/en-us/LC_MESSAGES/django.po translations/
-mv awx/ui/src/locales/en/messages.po translations/
+# mv awx/locale/en-us/LC_MESSAGES/django.po translations/
+# mv awx/ui/src/locales/en/messages.po translations/
 
 # Terminate docker
