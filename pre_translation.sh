3aa5f11c8834ced7e471f7bb4dc62e7ffc865e5d
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-16 14:38:27 -0400

    Update pre_translation.sh

--- pre_translation.sh
+++ pre_translation.sh
@@ -1,2 +1,7 @@
 
-echo "Test Pre Translation"
+# Extract Strings from API 
+make docker-compose-sources
+bash -c "docker-compose -f tools/docker-compose/_sources/docker-compose.yml run awx_1 make awx-link migrate po messages"
+
+# Move extracted Strings to Translation Directory
+mv awx/locale/en-us/LC_MESSAGES/django.po translations/
